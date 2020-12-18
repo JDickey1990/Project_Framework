83b29fe892d3bd23a37a7b01c9cf776906158029
jmd <jordan.michael.dickey@gmail.com>
2020-12-17 20:27:07 -0700

    changing the create action conditional block to redirect to login if login fails instead of rendering the new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to user_projects_path(@user)
         else
-            render :new
+            flash[:error] = "Login Failed"
+            redirect_to login_path
         end
     end
 
