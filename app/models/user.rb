62cb164593f8eb5ab0cda6e6cb3d2ee8857a5063
jmd <jordan.michael.dickey@gmail.com>
2020-12-17 19:00:20 -0700

    adding validations for my user model email and name attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :projects
 
-    validates_presence_of :email
+    validates :email, uniqueness: true, presence: true
     validates_presence_of :name
+    
     
 
 end
