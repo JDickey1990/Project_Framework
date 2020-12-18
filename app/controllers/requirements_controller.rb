0ff60da0dfb3593375c51c2700539b5d0127c280
jmd <jordan.michael.dickey@gmail.com>
2020-12-18 13:31:39 -0700

    adding a show action to requirements controller and a status action that changes the status of the requirement

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,3 +1,21 @@
 class RequirementsController < ApplicationController
  
+    def show 
+        @requirement = Requirement.find(params[:id])
+    end
+
+    def status
+        # binding.pry
+        @deadline = Deadline.find(params[:id])
+        if @deadline.completed == true
+            @deadline.completed = false
+            @deadline.save
+          else  @deadline.completed == false
+            @deadline.completed = true
+            @deadline.save
+          end
+          redirect_to user_project_path(@deadline.project.user_id,@deadline.project_id)
+    end
+
+   
 end
