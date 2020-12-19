52543e21b98c295d9d519de126dc1763d30cc647
jmd <jordan.michael.dickey@gmail.com>
2020-12-19 16:31:13 -0700

    adding a notes attribute to te requirements table

--- db/migrate/20201213022026_create_requirements.rb
+++ db/migrate/20201213022026_create_requirements.rb
@@ -1,7 +1,7 @@
 class CreateRequirements < ActiveRecord::Migration[6.0]
   def change
     create_table :requirements do |t|
-     t.datetime :deadline
+     t.string :notes
      t.string :description
      t.string :priority
       t.timestamps
