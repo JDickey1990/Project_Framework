8abeed14c5ab2ced46cff5b97a38c9b70850b854
jmd <jordan.michael.dickey@gmail.com>
2020-12-25 13:23:51 -0700

    adding the scope method for finding incomplete project deadlines

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -2,11 +2,5 @@ class Deadline < ApplicationRecord
     belongs_to :project
     belongs_to :requirement
 
-    # validates :deadline, presence: true
-
-     scope :incomplete_statuses, -> { where("completed == false") }
-
-    # Project.joins{:deadlines}.group{:deadline}.where("completed == false")
-
-    # scope :incomplete_statuses, -> { joins(:projects).where("completed == false") }
+    scope :incomplete_statuses, -> { where("completed == false") }
 end
