23a0a52de8d549041290f4ce113208d8b037b210
jmd <jordan.michael.dickey@gmail.com>
2021-01-04 11:40:00 -0700

    separating out concerns by creating a helper method for displaying strftime

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -3,4 +3,8 @@ class Deadline < ApplicationRecord
     belongs_to :requirement
 
     scope :incomplete_statuses, -> { where("completed == false") }
+    
+    def time
+        deadline.strftime("%A, %d %b %Y %l:%M %p")
+    end
 end
