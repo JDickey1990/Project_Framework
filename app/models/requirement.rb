cc93cf8c59141c8121ffb512464455f8bbfb8361
jmd <jordan.michael.dickey@gmail.com>
2021-01-04 11:49:37 -0700

    adding dependent destroy to delete children of parent objects

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,6 +1,6 @@
 class Requirement < ApplicationRecord
 has_many :deadlines
-has_many :projects, through: :deadlines
+has_many :projects, through: :deadlines, dependent: :destroy
 
 accepts_nested_attributes_for :deadlines
 
